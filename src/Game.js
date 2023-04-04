a2031f80ed53088a8de3a2a241417d560ffccbde
AustinYQM <a.r.mcgowan@gmail.com>
2023-04-04 16:32:14 -0500

    add files

---
+++ src/Game.js
@@ -0,0 +1,20 @@
+import { INVALID_MOVE } from "boardgame.io/core";
+export const ButtonMen = {
+  setup: () => ({
+    cells: Array(9).fill(null),
+  }),
+
+  turn: {
+    minMoves: 1,
+    maxMoves: 1,
+  },
+
+  moves: {
+    clickCell: ({ G, playerID }, id) => {
+      if (G.cells[id] !== null) {
+        return INVALID_MOVE;
+      }
+      G.cells[id] = playerID;
+    },
+  },
+};
